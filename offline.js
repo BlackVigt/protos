a284ec97ecf8491caa2e2ad9e07ad4566a2421a6
BlackVigt <blackvigt@gmail.com>
2017-06-20 00:20:40 -0500

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1497865085,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/piso.png",
+		"images/jugador-sheet0.png",
+		"images/mono1-sheet0.png",
+		"images/balaj-sheet0.png",
+		"images/glob-sheet0.png",
+		"images/balae-sheet0.png",
+		"images/voidglob-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
